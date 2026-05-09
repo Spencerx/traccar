ca9fc34d3f73e665bfd5e527c07131962f93458f
Anton Tananaev <anton@traccar.org>
2026-05-09 16:12:31 -0700

    Streaming KML data

--- src/main/java/org/traccar/reports/KmlExportProvider.java
+++ src/main/java/org/traccar/reports/KmlExportProvider.java
@@ -18,6 +18,7 @@ package org.traccar.reports;
 import org.traccar.helper.model.PositionUtil;
 import org.traccar.model.Device;
 import org.traccar.model.Geofence;
+import org.traccar.model.Position;
 import org.traccar.storage.Storage;
 import org.traccar.storage.StorageException;
 import org.traccar.storage.query.Columns;
@@ -29,7 +30,7 @@ import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
@@ -49,7 +50,6 @@ public class KmlExportProvider {
 
         var device = storage.getObject(Device.class, new Request(
                 new Columns.All(), new Condition.Equals("id", deviceId)));
-        var positions = PositionUtil.getPositions(storage, deviceId, from, to);
 
         Geofence geofence = geofenceId == 0 ? null : storage.getObject(Geofence.class, new Request(
                 new Columns.All(), new Condition.Equals("id", geofenceId)));
@@ -81,10 +81,16 @@ public class KmlExportProvider {
         writer.writeCharacters("absolute");
         writer.writeEndElement();
         writer.writeStartElement("coordinates");
-        writer.writeCharacters(positions.stream()
-                .filter(position -> geofence == null || geofence.containsPosition(position))
-                .map(p -> String.format("%f,%f,%f", p.getLongitude(), p.getLatitude(), p.getAltitude()))
-                .collect(Collectors.joining(" ")));
+        try (Stream<Position> positions = PositionUtil.getPositionsStream(storage, deviceId, from, to)
+                .filter(position -> geofence == null || geofence.containsPosition(position))) {
+            String separator = "";
+            for (var iterator = positions.iterator(); iterator.hasNext();) {
+                Position position = iterator.next();
+                writer.writeCharacters(separator + String.format(
+                        "%f,%f,%f", position.getLongitude(), position.getLatitude(), position.getAltitude()));
+                separator = " ";
+            }
+        }
         writer.writeEndElement();
         writer.writeEndElement();
         writer.writeEndElement();
