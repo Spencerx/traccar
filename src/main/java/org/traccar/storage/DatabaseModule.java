6cbba5742d238241154b590c1b5c9f4b33e7fb53
Anton Tananaev <anton@traccar.org>
2026-05-13 07:43:52 -0700

    Handle changelog directory (fix #5700)

--- src/main/java/org/traccar/storage/DatabaseModule.java
+++ src/main/java/org/traccar/storage/DatabaseModule.java
@@ -36,6 +36,7 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.URL;
+import java.nio.file.Path;
 
 public class DatabaseModule extends AbstractModule {
 
@@ -83,7 +84,9 @@ public class DatabaseModule extends AbstractModule {
         String changelog = config.getString(Keys.DATABASE_CHANGELOG);
         if (changelog != null && !changelog.isEmpty()) {
 
-            ResourceAccessor resourceAccessor = new DirectoryResourceAccessor(new File("."));
+            Path changelogPath = Path.of(changelog).toAbsolutePath().normalize();
+            ResourceAccessor resourceAccessor = new DirectoryResourceAccessor(changelogPath.getParent());
+            String changelogName = changelogPath.getFileName().toString();
 
             System.setProperty("liquibase.changelogLockWaitTimeInMinutes", "1");
             System.setProperty("liquibase.analytics.enabled", "false");
@@ -96,7 +99,7 @@ public class DatabaseModule extends AbstractModule {
                         config.getString(Keys.DATABASE_DRIVER),
                         null, null, null, resourceAccessor);
 
-                try (Liquibase liquibase = new Liquibase(changelog, resourceAccessor, database)) {
+                try (Liquibase liquibase = new Liquibase(changelogName, resourceAccessor, database)) {
                     liquibase.clearCheckSums();
                     liquibase.update(new Contexts());
                 }
