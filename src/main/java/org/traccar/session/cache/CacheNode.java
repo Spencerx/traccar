5eddf798819d68772ee924237b00084aa8ac8707
Anton Tananaev <anton@traccar.org>
2026-05-14 07:43:43 -0700

    Avoid unnecessary allocations

--- src/main/java/org/traccar/session/cache/CacheNode.java
+++ src/main/java/org/traccar/session/cache/CacheNode.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 - 2026 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.traccar.session.cache;
 
 import org.traccar.model.BaseModel;
@@ -26,14 +41,26 @@ public class CacheNode {
         this.value = value;
     }
 
-    public Set<CacheNode> getLinks(Class<? extends BaseModel> clazz, boolean forward) {
-        var map = forward ? links : backlinks;
-        return map.computeIfAbsent(clazz, k -> ConcurrentHashMap.newKeySet());
+    public Stream<CacheNode> linkStream(Class<? extends BaseModel> clazz, boolean forward) {
+        Set<CacheNode> set = (forward ? links : backlinks).get(clazz);
+        return set != null ? set.stream() : Stream.empty();
     }
 
     public Stream<CacheNode> getAllLinks(boolean forward) {
         var map = forward ? links : backlinks;
         return map.values().stream().flatMap(Set::stream);
+    }
+
+    public void addLink(Class<? extends BaseModel> clazz, boolean forward, CacheNode node) {
+        var map = forward ? links : backlinks;
+        map.computeIfAbsent(clazz, key -> ConcurrentHashMap.newKeySet()).add(node);
+    }
+
+    public void removeLink(Class<? extends BaseModel> clazz, boolean forward, CacheNode node) {
+        Set<CacheNode> set = (forward ? links : backlinks).get(clazz);
+        if (set != null) {
+            set.remove(node);
+        }
     }
 
 }
